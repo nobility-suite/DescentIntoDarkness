c3ee154f5430dc89becc5e96b2ac7b4397cae122
Joe <burtonjae@hotmail.co.uk>
2020-07-31 16:43:59 +0100

    Reindent files with tabs

--- src/main/java/com/gmail/sharpcastle33/did/generator/CaveGenContext.java
+++ src/main/java/com/gmail/sharpcastle33/did/generator/CaveGenContext.java
@@ -19,67 +19,67 @@ import java.util.Map;
 import java.util.Random;
 
 public class CaveGenContext implements AutoCloseable {
-    private final EditSession session;
-    public final CaveStyle style;
-    public final Random rand;
-    private boolean debug;
-    private final Map<BlockVector3, BlockState> blockCache = new HashMap<>();
+	private final EditSession session;
+	public final CaveStyle style;
+	public final Random rand;
+	private boolean debug;
+	private final Map<BlockVector3, BlockState> blockCache = new HashMap<>();
 
-    private CaveGenContext(EditSession session, CaveStyle style, Random rand) {
-        this.session = session;
-        this.style = style;
-        this.rand = rand;
-    }
+	private CaveGenContext(EditSession session, CaveStyle style, Random rand) {
+		this.session = session;
+		this.style = style;
+		this.rand = rand;
+	}
 
-    public CaveGenContext setDebug(boolean debug) {
-        this.debug = debug;
-        return this;
-    }
+	public CaveGenContext setDebug(boolean debug) {
+		this.debug = debug;
+		return this;
+	}
 
-    public boolean isDebug() {
-        return debug;
-    }
+	public boolean isDebug() {
+		return debug;
+	}
 
-    public static CaveGenContext create(World world, CaveStyle style, Random rand) {
-        EditSession session = WorldEdit.getInstance().getEditSessionFactory().getEditSession(world, -1);
-        return new CaveGenContext(session, style, rand);
-    }
+	public static CaveGenContext create(World world, CaveStyle style, Random rand) {
+		EditSession session = WorldEdit.getInstance().getEditSessionFactory().getEditSession(world, -1);
+		return new CaveGenContext(session, style, rand);
+	}
 
-    public boolean setBlock(BlockVector3 pos, BlockStateHolder<?> block) throws MaxChangedBlocksException {
-        if (pos.getBlockY() < 0 || pos.getBlockY() > 255) {
-            return false;
-        }
-        if (session.setBlock(pos, block)) {
-            blockCache.put(pos, block.toImmutableState());
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public boolean setBlock(BlockVector3 pos, BlockStateHolder<?> block) throws MaxChangedBlocksException {
+		if (pos.getBlockY() < 0 || pos.getBlockY() > 255) {
+			return false;
+		}
+		if (session.setBlock(pos, block)) {
+			blockCache.put(pos, block.toImmutableState());
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public BlockState getBlock(BlockVector3 pos) {
-        if (pos.getBlockY() < 0 || pos.getBlockY() > 255) {
-            return Util.requireDefaultState(BlockTypes.AIR);
-        }
-        return blockCache.computeIfAbsent(pos, session::getBlock);
-    }
+	public BlockState getBlock(BlockVector3 pos) {
+		if (pos.getBlockY() < 0 || pos.getBlockY() > 255) {
+			return Util.requireDefaultState(BlockTypes.AIR);
+		}
+		return blockCache.computeIfAbsent(pos, session::getBlock);
+	}
 
-    public Extent asExtent() {
-        return new AbstractDelegateExtent(session) {
-            @Override
-            public BlockState getBlock(BlockVector3 position) {
-                return CaveGenContext.this.getBlock(position);
-            }
+	public Extent asExtent() {
+		return new AbstractDelegateExtent(session) {
+			@Override
+			public BlockState getBlock(BlockVector3 position) {
+				return CaveGenContext.this.getBlock(position);
+			}
 
-            @Override
-            public <T extends BlockStateHolder<T>> boolean setBlock(BlockVector3 location, T block) throws WorldEditException {
-                return CaveGenContext.this.setBlock(location, block);
-            }
-        };
-    }
+			@Override
+			public <T extends BlockStateHolder<T>> boolean setBlock(BlockVector3 location, T block) throws WorldEditException {
+				return CaveGenContext.this.setBlock(location, block);
+			}
+		};
+	}
 
-    @Override
-    public void close() {
-        session.close();
-    }
+	@Override
+	public void close() {
+		session.close();
+	}
 }
--- src/main/java/com/gmail/sharpcastle33/did/generator/Centroid.java
+++ src/main/java/com/gmail/sharpcastle33/did/generator/Centroid.java
@@ -3,11 +3,11 @@ package com.gmail.sharpcastle33.did.generator;
 import com.sk89q.worldedit.math.Vector3;
 
 public class Centroid {
-    public final Vector3 pos;
-    public final int size;
+	public final Vector3 pos;
+	public final int size;
 
-    public Centroid(Vector3 pos, int size) {
-        this.pos = pos;
-        this.size = size;
-    }
+	public Centroid(Vector3 pos, int size) {
+		this.pos = pos;
+		this.size = size;
+	}
 }
