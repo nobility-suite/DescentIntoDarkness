e3d86c8dae8474ccf293dbaa0dca1335e9447027
Joe <burtonjae@hotmail.co.uk>
2020-12-13 06:32:35 +0000

    Add north, south, west, east to structure placement edge

--- src/main/java/com/gmail/sharpcastle33/did/generator/structure/StructurePlacementEdge.java
+++ src/main/java/com/gmail/sharpcastle33/did/generator/structure/StructurePlacementEdge.java
@@ -3,7 +3,15 @@ package com.gmail.sharpcastle33.did.generator.structure;
 import com.sk89q.worldedit.util.Direction;
 
 public enum StructurePlacementEdge {
-	FLOOR(Direction.DOWN), CEILING(Direction.UP), WALL(Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST);
+	FLOOR(Direction.DOWN),
+	CEILING(Direction.UP),
+	WALL(Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST),
+	NORTH(Direction.NORTH),
+	SOUTH(Direction.SOUTH),
+	WEST(Direction.WEST),
+	EAST(Direction.EAST),
+	;
+
 	private final Direction[] directions;
 
 	StructurePlacementEdge(Direction... directions) {
