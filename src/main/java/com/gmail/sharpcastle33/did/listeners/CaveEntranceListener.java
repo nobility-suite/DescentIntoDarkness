9e2a4858169bc78a8691a501747350f1730fdfb3
Stephen / Sharp <Sharpcastle33@users.noreply.github.com>
2021-02-07 02:53:06 -0500

    potential fix for cave villager teleport

--- src/main/java/com/gmail/sharpcastle33/did/listeners/CaveEntranceListener.java
+++ src/main/java/com/gmail/sharpcastle33/did/listeners/CaveEntranceListener.java
@@ -1,5 +1,6 @@
 package com.gmail.sharpcastle33.did.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.Listener;
@@ -14,16 +15,20 @@ public class CaveEntranceListener implements Listener {
 	public void interactEvent(PlayerInteractEntityEvent event) {
 		Entity e = event.getRightClicked();
 		if(e instanceof Villager) {
+			Bukkit.getLogger().info(event.getPlayer().getDisplayName() + "-- clicked villager");
 			Villager v = (Villager) e;
 			String name = v.getCustomName();
 			if(name.startsWith(ChatColor.BLUE + "Cave")) {
+				Bukkit.getLogger().info(event.getPlayer().getDisplayName() + "-- clicked cave villager");
+
 				String[] toParse = name.split("[");
 				String parse = toParse[1];
 				parse.replace("]", "");
 				int id = Integer.parseInt(parse);
 				
-				String[] args = {parse};
-				
+				String[] args = {"join",parse};
+				Bukkit.getLogger().info(event.getPlayer().getDisplayName() + "-- clicked cave villager, joining cave: " + parse);
+
 				CommandListener.join(event.getPlayer(), args);
 			}
 		}
